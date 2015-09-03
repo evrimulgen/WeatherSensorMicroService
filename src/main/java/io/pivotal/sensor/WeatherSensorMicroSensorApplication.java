2ec00a76dd0d5a17f10b46bc763e2c33bc39e99d
Martin O' Driscoll <modriscoll@pivotal.io>
2015-09-03 09:55:07 +0100

    Edited some properties + updated code

--- src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
+++ src/main/java/io/pivotal/sensor/WeatherSensorMicroSensorApplication.java
@@ -14,16 +14,14 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.DiscoveryClient;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 
